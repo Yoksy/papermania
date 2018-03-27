bffe56a457fc36d7f1d45aa6741bda66815ff4b8
Yoksy <thibault.duforest@gmail.com>
2018-03-27 17:13:02 +0200

    update api and store method + add authorId and authorImage to datas

--- src/api/crafts.js
+++ src/api/crafts.js
@@ -1,23 +1,44 @@
-import popularCrafts from '@/datas/tests/home/popular'
-import latestCrafts from '@/datas/tests/home/latest'
-import papercraftCrafts from '@/datas/tests/home/papercraft'
-import origamisCrafts from '@/datas/tests/home/origamis'
-import modelismCrafts from '@/datas/tests/home/modelism'
+import popularCrafts from '@/datas/tests/category/popular'
+import latestCrafts from '@/datas/tests/category/latest'
+import papercraftCrafts from '@/datas/tests/category/papercraft'
+import origamisCrafts from '@/datas/tests/category/origamis'
+import modelismCrafts from '@/datas/tests/category/modelism'
 
 export default {
   getCrafts(cb) {
     setTimeout(() => cb(popularCrafts), 100)
   },
 
-  async getHomepageCrafts(limit) {
+  async getCrafts(limit, category) {
     return new Promise(resolve => setTimeout(() => {
-      resolve({
-        popular: popularCrafts.slice(0, limit),
-        latest: latestCrafts.slice(0, limit),
-        papercraft: papercraftCrafts.slice(0, limit),
-        origamis: origamisCrafts.slice(0, limit),
-        modelism: modelismCrafts.slice(0, limit)
-      })
+      let response;
+
+      if (category) {
+        switch (category) {
+          case 'papercraft':
+            response = papercraftCrafts.slice(0, limit);
+            break;
+          case 'origamis':
+            response = origamisCrafts.slice(0, limit);
+            break;
+          case 'modelism':
+            response = modelismCrafts.slice(0, limit);
+            break;
+          default:
+            response = popularCrafts.slice(0, limit);
+            break;
+        }
+      } else {
+        response = {
+          popular: popularCrafts.slice(0, limit),
+          latest: latestCrafts.slice(0, limit),
+          papercraft: papercraftCrafts.slice(0, limit),
+          origamis: origamisCrafts.slice(0, limit),
+          modelism: modelismCrafts.slice(0, limit)
+        }
+      }
+
+      resolve(response)
     }, 100))
   }
 }
