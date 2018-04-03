5248e63700af2e6d257a4a181efa8e8de5820366
Yoksy <thibault.duforest@gmail.com>
2018-04-03 17:38:49 +0200

    add capitalizeFirstLetter and unhyphenate

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -5,3 +5,11 @@
 export function hyphenate(str) {
   return str.replace(/ /g,"-");
 }
+
+export function unhyphenate(str) {
+  return str.replace(/-/g," ");
+}
+
+export function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
